0a158e0752885a73a444f496832bfcbd2b48c985
root <seemugia1@gmail.com>
2018-12-12 17:30:53 +0700

    Fix editable row feature and delete row

--- controllers/main.py
+++ controllers/main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 import logging
 from odoo.addons.survey.controllers.main import WebsiteSurvey
+from odoo.exceptions import UserError
 _logger = logging.getLogger(__name__)
 
 class websitesurvey(WebsiteSurvey):
@@ -12,18 +13,50 @@ class websitesurvey(WebsiteSurvey):
                 type='http', auth='public', website=True)
     def prefill(self, survey, token, page=None, **post):
         UserInputLine = request.env['survey.user_input_line']
+        Label = request.env['survey.label']
         ret = {}
         # Fetch previous answers
         if page:
             previous_answers = UserInputLine.sudo().search([('user_input_id.token', '=', token), ('page_id', '=', page.id)])
+            previous_label = Label.sudo().search([('user_input_id_2.token', '=', token)])
         else:
             previous_answers = UserInputLine.sudo().search([('user_input_id.token', '=', token)])
+            previous_label = Label.sudo().search([('user_input_id_2.token', '=', token)])
         # Return non empty answers in a JSON compatible format
+        for label in previous_label:
+            answer_tag_label = '%s_%s_%s_%s_%s' % (label.user_input_id_2.survey_id.id, label.question_id_2.page_id.id, label.question_id_2.id, label.user_input_id_2.token, label.id)
+            ret.setdefault(answer_tag_label, []).append(label.value)
         for answer in previous_answers:
             if not answer.skipped:
                 answer_tag = '%s_%s_%s' % (answer.survey_id.id, answer.page_id.id, answer.question_id.id)
                 answer_value = None
                 if answer.question_id.matrix_subtype == 'custom_row':
+                    if answer.answer_type == 'free_text':
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_free_text
+                    elif answer.answer_type == 'text':
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_text
+                    elif answer.answer_type == 'number':
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = str(answer.value_number)
+                    elif answer.answer_type == 'date':
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_date
+                    elif answer.answer_type == 'dropdown':
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_dropdown.id
+                    elif answer.answer_type == 'suggestion' and not answer.value_suggested_row:
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_suggested.id
+                    elif answer.answer_type == 'suggestion' and answer.value_suggested_row:
+                        answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
+                        answer_value = answer.value_suggested.id
+                    if answer_value:
+                        ret.setdefault(answer_tag, []).append(answer_value)
+                    else:
+                        _logger.warning("[survey] No answer has been found for question %s marked as non skipped" % answer_tag)
+                elif answer.question_id.matrix_subtype == 'editable_row':
                     if answer.answer_type == 'free_text':
                         answer_tag = "%s_%s_%s" % (answer_tag, answer.value_suggested_row.id, answer.value_suggested.id)
                         answer_value = answer.value_free_text
--- models/survey.py
+++ models/survey.py
@@ -11,9 +11,19 @@ def dict_keys_startswith(dictionary, string):
     """
     return {k: v for k, v in dictionary.items() if k.startswith(string)}
 
+def dict_keys_endswith(dictionary, string):
+    return {k: v for k, v in dictionary.items() if k.endswith(string)}
+
 def dict_keys_endswith_line(dictionary, string1, string2):
     """ Return dict for user input line, which search the value from the string (ends with string AND not start with string)"""
     return {k: v for k, v in dictionary.items() if k.endswith(string1) and not k.startswith(string2)}
+
+def dict_remove_unneeded_keys(dictionary):
+    """ remove [0] which is a dummy"""
+    return {k: v for k, v in dictionary.items() if not k.endswith('[0]')}
+
+def dict_remove_unneeded_vals(dictionary):
+    return {k: v for k, v in dictionary.items() if not v == ''}
 
 class SurveyQuestion(models.Model):
     _inherit = 'survey.question'
@@ -32,6 +42,8 @@ class SurveyLabel(models.Model):
             ], string='Type of Question', default="checkbox")
 
     dpvalues = fields.Many2many('dp.attributes.value', string="values")
+    user_input_id = fields.Many2one('survey.user_input', string="Column")
+    user_input_id_2 = fields.Many2one('survey.user_input', string="Row")
 
 class SurveyLabelManyTags(models.Model):
     _name = 'dp.attributes.value'
@@ -52,16 +64,16 @@ class SurveyUserInputLine(models.Model):
     matrix_subtype_id = fields.Selection(related='question_id.matrix_subtype', string="Matrix subtype")
 
     @api.model
-    def save_line_matrix(self, user_input_id, question, post, answer_tag):
+    def save_line_matrix(self, user_input_id, question, post, answer_tag, survey_id):
         vals = {
             'user_input_id': user_input_id,
             'question_id': question.id,
-            'survey_id': question.survey_id.id,
+            'survey_id': survey_id,
             'skipped': False
         }
         old_uil = self.search([
             ('user_input_id', '=', user_input_id),
-            ('survey_id', '=', question.survey_id.id),
+            ('survey_id', '=', survey_id),
             ('question_id', '=', question.id)
         ])
         old_uil.sudo().unlink()
@@ -116,40 +128,76 @@ class SurveyUserInputLine(models.Model):
         elif question.matrix_subtype == 'editable_row':
             label_dict = dict_keys_startswith(post, 'label[')
             answer_lines = dict_keys_endswith_line(post, ']', 'label[')
-            i = 0
+            label_dict = dict_remove_unneeded_keys(label_dict)
+            answer_lines = dict_remove_unneeded_keys(answer_lines)
+            answer_lines = dict_remove_unneeded_vals(answer_lines)
+            label_dict = dict_remove_unneeded_vals(label_dict)
+            i = 1
             for key, val in label_dict.items():
                 label_vals = {
                     'value': val,
+                    'type': 'checkbox',
                     'question_id_2': question.id,
-                    'type': 'checkbox'
+                    'user_input_id_2': user_input_id
                 }
+                # if key != 'label[0]':
+                #     label = self.env['survey.label'].create(label_vals)
                 label = self.env['survey.label'].create(label_vals)
-                for col in question.labels_ids:
-                    a_tag = "%s_%s_%s" % (answer_tag, label.id, col.id)
-                    col_vals = {k: v for k, v  in  answer_lines.items() if k.endswith('['+str(i)+']')}
-                    for col_val in col_vals:
-                        if a_tag in ca_dict:
-                            # to fix
-                            no_answers = False
-                            if post.get(a_tag): 
-                                sline = a_tag.split('_')[-1]
-                                label_obj = question.labels_ids.browse(int(sline))
-                                if label_obj.type == 'textbox':
-                                    vals.update({'answer_type': 'text', 'value_suggested': col.id, 'value_suggested_row': row.id, 'value_text': post.get(a_tag)})
-                                elif label_obj.type == 'free_text':
-                                    vals.update({'answer_type': 'free_text', 'value_suggested': col.id, 'value_suggested_row': row.id, 'value_free_text': post.get(a_tag)})
-                                elif label_obj.type == 'numerical_box':
-                                    vals.update({'answer_type': 'number', 'value_suggested': col.id, 'value_suggested_row': row.id, 'value_number': post.get(a_tag)})
-                                elif label_obj.type == 'date':
-                                    vals.update({'answer_type': 'date', 'value_suggested': col.id, 'value_suggested_row': row.id, 'value_date': post.get(a_tag)})
-                                elif label_obj.type == 'dropdown':
-                                    vals.update({'answer_type': 'dropdown', 'value_suggested': col.id, 'value_suggested_row': row.id, 'value_dropdown': int(post.get(a_tag))})
-                                else:
-                                    vals.update({'answer_type': 'suggestion', 'value_suggested': col.id, 'value_suggested_row': row.id})
-                                self.create(vals)
+                col_vals = {k: v for k, v  in  answer_lines.items() if k.endswith('['+str(i)+']')}
+                col_test = ()
+                for col_key,col_val in sorted(col_vals.items()):
+                    # to fix
+                    no_answers = False
+                    col_obj = ""
+                    if col_val: 
+                        # col_obj = re.sub("["+str(i)+"]","",col_key)
+                        for c in col_key:
+                            if c == '[':
+                                break
+                            col_obj += c
+                        col_obj = col_obj.split('[')[0].split('_')[-1]
+                        label_obj = self.env['survey.label'].search([('id', '=', col_obj)])
+                        if label_obj.type == 'textbox':
+                            vals.update({'answer_type': 'text', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_text': col_val})
+                        elif label_obj.type == 'free_text':
+                            vals.update({'answer_type': 'free_text', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_free_text': col_val})
+                        elif label_obj.type == 'numerical_box':
+                            vals.update({'answer_type': 'number', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_number': float(col_val)})
+                        elif label_obj.type == 'date':
+                            vals.update({'answer_type': 'date', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_date': col_val})
+                        elif label_obj.type == 'dropdown':
+                            vals.update({'answer_type': 'dropdown', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_dropdown': int(col_val)})
+                        else:
+                            vals.update({'answer_type': 'suggestion', 'value_suggested': col_obj, 'value_suggested_row': label.id})
+                        self.create(vals)
                 i += 1
-
-
+            row_post = {}
+            test = ()
+            row_label = self.env['survey.label'].search([('question_id_2', '=', question.id), ('user_input_id_2', '=', user_input_id)])
+            for rl in row_label.ids:
+                for k, v in post.items():
+                    try:
+                        if rl == int(k.split('[')[0].split('_')[-1]):
+                            rl = self.env['survey.label'].search([('id', '=', rl)])
+                            rl.write({'value': v})
+                        if rl == int(k.split('_')[-2]):
+                            col_obj = self.env['survey.label'].search([('id', '=', int(k.split('_')[-1]))])
+                            if col_obj.type == 'textbox':
+                                vals.update({'answer_type': 'text', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_text': col_val})
+                            elif col_obj.type == 'free_text':
+                                vals.update({'answer_type': 'free_text', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_free_text': col_val})
+                            elif col_obj.type == 'numerical_box':
+                                vals.update({'answer_type': 'number', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_number': float(col_val)})
+                            elif col_obj.type == 'date':
+                                vals.update({'answer_type': 'date', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_date': col_val})
+                            elif col_obj.type == 'dropdown':
+                                vals.update({'answer_type': 'dropdown', 'value_suggested': col_obj, 'value_suggested_row': label.id, 'value_dropdown': int(col_val)})
+                            else:
+                                vals.update({'answer_type': 'suggestion', 'value_suggested': col_obj, 'value_suggested_row': label.id})
+                            self.create(vals)
+                    except ValueError:
+                        continue
+            raise UserError((test))
         if no_answers:
             vals.update({'answer_type': None, 'skipped': True})
             self.create(vals)
